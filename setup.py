7fe08ff3900dbed164c4e28c895f5055051f493c
Ekventor <78481240+Ekventor@users.noreply.github.com>
2021-02-05 14:06:58 +0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ requirements = ["asyncio>=3", "aiohttp>=3"]
 
 setup(
     name="async_VKsher",
-    version="1.0.0",
+    version="1.1.1",
     author="Kulenov Islam",
     author_email="kit.werr34@gmail.com",
     description="asyncVK is asynchronous library for creating a bot in VK",
