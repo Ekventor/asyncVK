3650d8d76bbe720e1bca32c7e6192d5b3fded116
Ekventor <78481240+Ekventor@users.noreply.github.com>
2022-07-25 19:43:17 +0600

    Add files via upload

---
+++ asyncVK/chain.py
@@ -0,0 +1,42 @@
+from typing import List, Tuple
+from copy import deepcopy
+from .handlers import Handler
+
+
+class BoundChain:
+    def __init__(self, handlers: List[Handler], user_id: int):
+        self.handlers = handlers
+        self.user_id = user_id
+
+    def is_trigger(self, event_type: str, user_id: int) -> bool:
+        return event_type == self.handlers[0].event_type and user_id == self.user_id
+
+    async def new_event(self, token: str, event: dict, event_params: dict) -> Tuple:
+        result = None
+
+        handler = self.handlers.pop(0)
+        if handler.is_trigger(event_params):
+            await handler.new_event(token, event, event_params)
+            result = True
+        else:
+            result = False
+
+        if len(self.handlers) < 1:
+            result = False
+
+        return result, self
+
+
+class Chain:
+    def __init__(self):
+        self.handlers = []
+
+    def add_handler(self, handler: Handler) -> Handler:
+        self.handlers.append(handler)
+        return handler
+
+    def is_trigger(self, event_params: dict) -> bool:
+        return self.handlers[0].is_trigger(event_params)
+
+    def bind_chain(self, user_id: int) -> BoundChain:
+        return BoundChain(deepcopy(self.handlers), user_id)
